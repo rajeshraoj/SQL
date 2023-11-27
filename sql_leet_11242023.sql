0733998caba7bb6ce41e4faed73a4539664fcf50
rajeshraoj <rajesh_jrao@yahoo.com>
2023-11-27 06:39:54 -0600

    completed more

--- sql_leet_11242023.sql
+++ sql_leet_11242023.sql
@@ -1,179 +1,73 @@
+###################### 1294
+
 SELECT
-    D.dept_name,
-    COALESCE(COUNT(student_id)) student_number
-FROM department D
-LEFT OUTER JOIN Student S ON (D.dept_id = S.dept_id)
-GROUP BY 1
-ORDER BY 2 DESC,1
+    C.country_name,
+    CASE
+        WHEN AVG(weather_state) <= 15 THEN 'Cold'
+        WHEN AVG(weather_state) >= 25 THEN 'Hot'
+        ELSE 'Warm' END Weather_type
 
-SELECT dept_name, SUM(CASE WHEN s.dept_id THEN 1 ELSE 0 END) as student_number
-FROM department d LEFT JOIN student s ON d.dept_id = s.dept_id
-GROUP BY 1
-ORDER BY 2 desc, 1g
+FROM Countries C
+INNER JOIN Weather W ON (C.country_id = W.country_id)
+WHERE DATE_FORMAT(W.day,'%Y-%m') = '2019-11'
+GROUP BY C.country_name
 
-#################################### 2112
-WITH F1 AS
-(SELECT
-    departure_airport airport,
-    SUM(flights_count) dfc
-FROM Flights
-GROUP BY 1),
-F2 AS (SELECT
-    arrival_airport airport,
-    SUM(flights_count) afc
-FROM Flights
-GROUP BY 1),
-cte AS (
-SELECT 
-    F1.airport,
-    coalesce(dfc,0) + coalesce(afc,0) cnt
-FROM F1 LEFT OUTER JOIN F2 ON (F1.airport = F2.airport)
-UNION
-SELECT 
-    F2.airport,
-    coalesce(dfc,0) + coalesce(afc,0) cnt
-FROM F1 RIGHT OUTER JOIN F2 ON (F1.airport = F2.airport))
-SELECT airport as airport_id FROM cte WHERE cnt = (select max(cnt) from cte)
-
-WITH CTE AS (
-  SELECT airport_id, RANK() OVER(ORDER BY SUM(flights_count) DESC) AS RN
-  FROM ( SELECT departure_airport AS airport_id, flights_count FROM Flights
-           UNION ALL
-       SELECT arrival_airport AS airport_id, flights_count FROM Flights ) T
-  GROUP BY airport_id )
-
-SELECT airport_id FROM CTE WHERE RN = 1
-
-
-###################################### 1445
-
+###  My answer does not take care of duplicates in country name, better to join based on country_id then join with main country table
 # Write your MySQL query statement below
-SELECT
-    A.sale_date,
-    app_num - orj_num diff
-FROM 
-(SELECT
-    sale_date,
-    sold_num as app_num
-FROM Sales
-WHERE fruit = 'apples'
-GROUP BY 1) A 
-INNER JOIN (SELECT
-                sale_date,
-                sold_num as orj_num
-            FROM Sales
-            WHERE fruit = 'oranges'
-            GROUP BY 1) O 
-ON A.sale_date = O.sale_date
+SELECT c.country_name, avg_weather.weather_type
+FROM Countries as c
+JOIN
+(SELECT country_id, AVG(weather_state) as "avg_weather",
+CASE 
+    WHEN AVG(weather_state) <= 15 THEN "Cold"
+    WHEN AVG(weather_state) >= 25 THEN "Hot"
+    ELSE "Warm"
+END as weather_type
+FROM Weather
+WHERE day BETWEEN "2019-11-01" AND "2019-11-30"
+GROUP BY country_id) as avg_weather
+ON c.country_id = avg_weather.country_id;
 
-##### Super
 
-select sale_date, sum(if(fruit='apples', sold_num, 0)) -  sum(if(fruit='oranges', sold_num, 0))  as diff
-from sales
-group by sale_date
-
-################### 534
+#######################################33 2339
 
 SELECT
-    player_id,
-    event_date,
-    SUM(games_played) OVER(PARTITION BY player_id ORDER BY event_date) games_played_so_far
-FROM Activity
+    H.team_name AS home_team,
+    A.team_name AS away_team
+FROM Teams H
+CROSS JOIN Teams A
+WHERE H.team_name != A.team_name
 
-#######################3 2228
+############################################### 2142
 
-WITH cte AS (
 SELECT
-    *,
-    IF(DATEDIFF(LEAD(purchase_date,1) OVER(PARTITION BY user_id ORDER BY purchase_date),purchase_date) <= 7,1,0)  sel
-FROM Purchases)
+    B.bus_id,
+    COUNT(X.passenger_id) passengers_cnt
+FROM Buses B
+LEFT OUTER JOIN (
 SELECT
-    DISTINCT user_id
-FROM cte WHERE sel = 1
+    P.passenger_id,
+    min(B.arrival_time) bus_arrival_time
+FROM Buses B
+CROSS JOIN Passengers P
+WHERE P.arrival_time <= B.arrival_time
+GROUP BY P.passenger_id ) X
+ON (B.arrival_time = X.bus_arrival_time)
+GROUP BY 1
 ORDER BY 1
 
-########### Using Self Join
-select distinct p1.user_id
-from purchases p1
-inner join
-purchases p2
-on p1.user_id=p2.user_id and p1.purchase_id<>p2.purchase_id
-and abs(datediff(p1.purchase_date, p2.purchase_date))<=7
-order by p1.user_id
-
-
-######################  1132
-
-WITH cte AS (
-SELECT 
- *
-FROM (
-SELECT
- post_id,
- action_date
-FROM Actions
-WHERE action = 'report' and extra = 'spam') A
-LEFT OUTER JOIN (
-    SELECT post_id as rem
-    FROM Removals
-) B ON (A.post_id = B.rem)),
-cte2 as (
-SELECT
-    count(distinct rem)/COUNT(distinct post_id) cnt from cte
-    group by action_date)
-SELECT round(avg(cnt)*100,2) average_daily_percent from cte2
-
-
-#################################### 1098
-
-select b.book_id, b.name
-from books b left join
-    (select book_id, sum(quantity) as book_sold
-    from Orders 
-    where dispatch_date between '2018-06-23' and '2019-06-23'
-    group by book_id) t
-on b.book_id = t.book_id
-where available_from < '2019-05-23'
-and (book_sold is null or book_sold <10)
-order by b.book_id;
-
-
-######################## 2308
+#################3333 1988
 
 WITH cte AS (
 SELECT
-    user_id,
-    gender,
-    CASE 
-        WHEN gender='female' then 1 
-        WHEN gender='male'  then 3
-        ELSE 2 END ordr,
-    RANK() OVER(partition by gender order by user_id) rnk
-FROM Genders
-)
-SELECT 
-    user_id,
-    gender
-FROM cte
-ORDER BY rnk, ordr
-
-################ 1285
-
-SELECT log_id - num,min(log_id) as start_id, max(log_id) as end_id
-FROM
-(SELECT log_id, ROW_NUMBER() OVER(ORDER BY log_id) as num
-FROM Logs) a
-GROUP BY log_id - num
-
-
-##########33 2324
-
-WITH cte AS (
+    school_id,
+    max(student_count),
+    min(score) score
+FROM Schools
+CROSS JOIN Exam
+WHERE capacity >= student_count
+GROUP BY school_id)
 SELECT
-    U.user_id,
-    U.product_id,
-    rank() over(PARTITION BY U.user_id ORDER BY sum(U.quantity * P.price)DESC) rnk
-FROM Sales U
-INNER JOIN Product P ON (U.product_id = P.product_id)
-GROUP BY 1,2)
-SELECT user_id, product_id FROM cte WHERE rnk = 1
+    S.school_id,
+    coalesce(cte.score,-1) score
+FROM Schools S LEFT OUTER JOIN cte on(S.school_id = cte.school_id)
