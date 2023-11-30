c2632e24cf32bcf58c1c37d414f58ed425b6c9c5
rajeshraoj <rajesh_jrao@yahoo.com>
2023-11-29 18:04:45 -0600

    made further updates

--- sql_leet_11292023.sql
+++ sql_leet_11292023.sql
@@ -11,3 +11,15 @@ SELECT id,
             WHEN child_count.p_id IS NULL THEN 'Leaf'
             ELSE 'Inner' END type FROM Tree T
 LEFT OUTER JOIN child_count ON (T.id = child_count.p_id)
+
+WITH cte AS (
+SELECT
+    user_id,
+    visit_date,
+    DATEDIFF(COALESCE(LEAD(visit_date,1) OVER(PARTITION BY user_id ORDER BY visit_date),'2021-01-01'),visit_date) biggest_window
+FROM UserVisits)
+SELECT 
+    user_id,
+    max(biggest_window) AS biggest_window
+FROM cte
+GROUP BY user_id
